14e6e60ede793bd356ba355f3aaa7d02d8b5f668
andregoudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-05-07 12:32:42 -0300

    add - fiz alteração do que tinha no botoes.js

--- script/botoes.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector("form");
-
-const botaoCancelar = document.querySelector(".btn-outline-secondary");
-
-botaoCancelar.addEventListener("click", function () {
-    form.reset();
-});
